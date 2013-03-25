8d43f00fcacc7af1a7d6736591059cc2a2b06934
Josh Matthias <jmatthias@lacoda.com>
2013-03-24 23:45:48 -0400

    Tree-scan tests
    Add tests to confirm that 'scan_api_tree' correctly adds all views and
    routes in an API tree.

--- tests/test_apitree.py
+++ tests/test_apitree.py
@@ -5,10 +5,32 @@ from pyramid_apitree import (
     scan_api_tree,
     )
 
+""" An example API tree.
+    
+    api_tree = {
+        'GET': root_get,
+        '/resource': {
+            'GET': resource_get_all,
+            'POST': resource_post,
+            '.{resource_id}': {
+                'GET': resource_individual_get,
+                'PUT': resource_individual_update,
+                'DELETE': resource_individual_delete,
+                '/component': {
+                    'GET': resource_individual_component_get,
+                    }
+                }
+            }
+        }
+    
+    """
+
 def make_request_methods_tuple(request_method):
     """ The Pyramid 'Configurator' 'add_view' and 'add_route' methods allow the
         'request_method' to either be a string value or a tuple of string
         values. This function duplicates that behavior. """
+    ALL_REQUEST_METHODS = ('GET', 'POST', 'PUT', 'DELETE', 'HEAD')
+    
     if isinstance(request_method, tuple):
         return request_method
     
@@ -19,8 +41,6 @@ def make_request_methods_tuple(request_method):
 
 class MockConfigurator(object):
     """ A mocked Pyramid configurator. """
-    ALL_REQUEST_METHODS = ('GET', 'POST', 'PUT', 'DELETE', 'HEAD')
-    
     def __init__(self):
         self.views = {}
         self.routes = set()
@@ -53,23 +73,23 @@ def test_empty_api_tree():
     assert not config.views
     assert not config.routes
 
-class ScanAPITreeTest(unittest.TestCase):
-    root_path = '/'
+class ScanTest(unittest.TestCase):
+    def target(*pargs, **kwargs):
+        """ A dummy 'view_callable' object. Used as a target for tests. """
     
-    def setUp(self):
-        def dummy(*pargs, **kwargs):
-            """ A dummy 'view callable' object. """
-            pass
-        
-        self.dummy = dummy
+    def dummy(*pargs, **kwargs):
+        """ Another dummy 'view_callable' object. """
     
-    def endpoint_test(self, path, request_method=None, **expected):
-        expected.setdefault('view_callable', self.dummy)
+    def endpoint_test(self, path, request_method=None, expected={}):
+        expected['view_callable'] = self.target
         
         request_methods = make_request_methods_tuple(request_method)
         
         config = MockConfigurator()
-        scan_api_tree(config, self.api_tree, self.root_path)
+        scan_api_tree(
+            configurator=config,
+            api_tree=self.api_tree,
+            root_path='')
         
         assert path in config.routes
         assert path in config.views
@@ -78,27 +98,90 @@ class ScanAPITreeTest(unittest.TestCase):
             view_dict = config.views[path][item]
             assert view_dict == expected
 
-class TestEmptyAPITree(ScanAPITreeTest):
-    def request_method_test(self, request_method):
-        self.api_tree = {request_method: self.dummy}
-        self.endpoint_test('/', request_method=request_method)
+class TestRequestMethods(ScanTest):
+    def request_method_test(self, request_method=None):
+        self.api_tree = {request_method: self.target}
+        # Use an empty string ('') for root.
+        self.endpoint_test(path='', request_method=request_method)
     
-    def test_GET_endpoint(self):
-        self.request_method_test('GET')
+    def test_no_request_method(self):
+        self.request_method_test()
     
-    def test_POST_endpoint(self):
-        self.request_method_test('POST')
+    def test_yes_request_method(self):
+        self.request_method_test(request_method='GET')
     
-    def test_PUT_endpoint(self):
-        self.request_method_test('PUT')
+    def test_multiple_request_methods(self):
+        self.request_method_test(request_method=('GET', 'POST'))
+
+class TestRequestMethodsMultipleEndpoints(ScanTest):
+    def test_multiple_endpoints(self):
+        self.api_tree = {
+            'GET': self.dummy,
+            'POST': self.target,
+            }
+        self.endpoint_test(path='', request_method='POST')
+
+class TestBranch(ScanTest):
+    def test_branch(self):
+        self.api_tree = {'/resource': self.target}
+        self.endpoint_test('/resource')
     
-    def test_DELETE_endpoint(self):
-        self.request_method_test('DELETE')
+    def test_branch_request_method(self):
+        self.api_tree = {
+            '/resource': {
+                'GET': self.target,
+                }
+            }
+        self.endpoint_test('/resource', 'GET')
     
-    def test_HEAD_endpoint(self):
-        self.request_method_test('HEAD')
+    def test_multiple_branches(self):
+        self.api_tree = {
+            '/other_resource': self.dummy,
+            '/resource': self.target,
+            }
+        self.endpoint_test('/resource')
+
+class TestComplexBranch(ScanTest):
+    def test_branch_branch(self):
+        self.api_tree = {
+            '/resource': {
+                '/component': self.target
+                }
+            }
+        self.endpoint_test('/resource/component')
     
+    def test_branch_branch_request_method(self):
+        self.api_tree = {
+            '/resource': {
+                '/component': {
+                    'GET': self.target
+                    }
+                }
+            }
+        self.endpoint_test('/resource/component', request_method='GET')
     
+    def test_branch_multiple_branches(self):
+        self.api_tree = {
+            '/resource': {
+                '/other_component': self.dummy,
+                '/component': self.target,
+                }
+            }
+        self.endpoint_test('/resource/component')
+
+class TestViewKwargs(ScanTest):
+    def target(*pargs, **kwargs):
+        """ A view callable with a 'view_kwargs' attribute. The API tree scan
+            should automatically unpack the 'view_kwargs' when calling
+            'configurator.add_view()'. """
+    
+    target.view_kwargs = {'predicate': 'predicate value'}
+    
+    def test_view_kwargs(self):
+        self.api_tree = {
+            '': self.target,
+            }
+        self.endpoint_test('', expected=self.target.view_kwargs)
     
     
 
