30e5fccee99daf1f04bdec1bb2e8e794042d024d
Josh Matthias <jmatthias@lacoda.com>
2013-03-26 11:05:40 -0400

    Implement 'simple_view'
    Implement 'simple_view' decorator.
    
    All tests pass.

--- pyramid_apitree/view_callable.py
+++ pyramid_apitree/view_callable.py
@@ -1,2 +1,11 @@
 def simple_view(*pargs, **kwargs):
-    pass
+    if pargs:
+        wrapped = pargs[0]
+        wrapped.view_kwargs = {}
+        return wrapped
+    
+    def inner_decorator(wrapped):
+        wrapped.view_kwargs = kwargs
+        return wrapped
+    
+    return inner_decorator
